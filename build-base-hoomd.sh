e60b109c634c3c30e44f561ffd3259116ce51c9d
Ian Graham <irgraham1@gmail.com>
2021-11-15 08:46:18 -0500

    moved variables to .env

--- build-base-hoomd.sh
+++ build-base-hoomd.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# will have to not hardcode in the future
-NUM_THREADS=6
-ENABLE_GPU=ON
-
 cmake -B .build/hoomd -S hoomd-blue -DENABLE_GPU=$ENABLE_GPU
 
-cmake --build .build/hoomd -j$NUM_THREADS
+cmake --build .build/hoomd -j$BUILD_THREADS
 
 cmake --install .build/hoomd
--- build-hoomd-plugins.sh
+++ build-hoomd-plugins.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# will have to not hardcode in the future
-NUM_THREADS=6
-ENABLE_GPU=ON
-
 cmake -B .build/plugins -S plugins -DENABLE_GPU=$ENABLE_GPU
 
-cmake --build .build/plugins -j$NUM_THREADS
+cmake --build .build/plugins -j$BUILD_THREADS
 
 cmake --install .build/plugins

9dea5418e7dc35bcf75b18664e70d15efaaa174b
Ian Graham <irgraham1@gmail.com>
2022-02-25 10:10:26 -0500

    adding new helper functions to monk

--- src/monk/pair.py
+++ src/monk/pair.py
@@ -4,14 +4,6 @@ import hoomd
 import hoomd.pair_plugin.pair as p_pair
 import numpy as np
 
-# This is all garbage (at the moment)!
-# There is something wrong with the Table implementation on GPUs
-# in HOOMDv3
-
-# Anyway, I should be writing my own kernels
-# The Table incurs a pretty big slowdown
-
-
 def WCA_pot_smooth(r, rmin, rmax, epsilon, sigma):
     V = 4 * epsilon * ((sigma / r)**12 - (sigma / r)**6) + \
         epsilon - epsilon * (36*2**(-1/3.)*(r-rmax)**2) / sigma**2
@@ -40,27 +32,6 @@ def hertz_pot(r, rmin, rmax, sigma):
     V = 0.4*np.power(1-r/sigma, 2.5)
     F = (1/sigma)*np.power(1-r/sigma, 1.5)
     return (V, F)
-
-
-def table_params(width: int, pot_func, r_min: float, r_max:float, coeff=None) -> Dict:
-    '''Helper function to make assigning table params in HOOMD v3 similar to how
-    it was done in v2.
-    '''
-    r = np.linspace(r_min, r_max, width, endpoint=False)
-    (V, F) = pot_func(r, r_min, r_max, **coeff)
-    # print(len(V), len(F))
-    return dict(r_min=r_min, V=V, F=F)
-
-def update_params(pair: hoomd.md.pair.Pair, kwargs=None):
-        
-    # print(list(pair.params.items()))
-
-    for k, v in pair.params.items():
-        v.update(kwargs)
-
-    pair._update_param_dict()
-
-    # print(list(pair.params.items()))
 
 
 def KA_LJ(nlist: hoomd.md.nlist.NList) -> hoomd.md.pair.Pair:
@@ -137,92 +108,6 @@ def LJ1208(nlist: hoomd.md.nlist.NList) -> hoomd.md.pair.Pair:
     lj.r_on[('B', 'B')] = r_on_cutoff*sig_BB
 
     return lj
-
-# TODO seems to be broken
-def WCA(nlist: hoomd.md.nlist.NList) -> hoomd.md.pair.Pair:
-    '''WCA potential
-    '''
-    r_cutoff = 2**(1./6)
-    r_min = 0.722462048309373
-    WIDTH = 1000  # number of particle pairs
-
-    eps_AA = 1
-    eps_AB = 1.5
-    eps_BB = 0.5
-    sig_AA = 1
-    sig_AB = 0.8
-    sig_BB = 0.88
-    # specify WCA interactions between particle pairs
-    table = hoomd.md.pair.Table(nlist=nlist)
-
-    table.params[('A', 'A')] = table_params(
-        WIDTH,
-        WCA_pot_smooth,
-        r_min*sig_AA,
-        r_cutoff*sig_AA,
-        coeff=dict(epsilon=eps_AA, sigma=sig_AA)
-    )
-    table.r_cut[('A', 'A')] = r_cutoff*sig_AA
-    table.params[('A', 'B')] = table_params(
-        WIDTH,
-        WCA_pot_smooth,
-        r_min*sig_AB,
-        r_cutoff*sig_AB,
-        coeff=dict(epsilon=eps_AB, sigma=sig_AB)
-    )
-    table.r_cut[('A', 'B')] = r_cutoff*sig_AB
-    table.params[('B', 'B')] = table_params(
-        WIDTH,
-        WCA_pot_smooth,
-        r_min*sig_BB,
-        r_cutoff*sig_BB,
-        coeff=dict(epsilon=eps_BB, sigma=sig_BB)
-    )
-    table.r_cut[('B', 'B')] = r_cutoff*sig_BB
-    return table
-
-# TODO seems to be broken
-def mLJ(nlist: hoomd.md.nlist.NList, delta: float) -> hoomd.md.pair.Pair:
-    '''Kob-Anderson Lennard-Jones potential with modified well width
-    '''
-    r_cutoff = 2.5
-    r_min = 0.722462048309373
-    WIDTH = 1000
-
-    eps_AA = 1
-    eps_AB = 1.5
-    eps_BB = 0.5
-    sig_AA = 1
-    sig_AB = 0.8
-    sig_BB = 0.88
-    # specify mLJ interactions between particle pairs using pair.Table
-    table = hoomd.md.pair.Table(nlist=nlist)
-
-    table.params[('A', 'A')] = table_params(
-        WIDTH,
-        mLJ_pot_force_shifted,
-        r_min*sig_AA,
-        r_cutoff*sig_AA,
-        coeff=dict(epsilon=eps_AA, sigma=sig_AA, delta=delta)
-    )
-    table.r_cut[('A', 'A')] = r_cutoff*sig_AA
-    table.params[('A', 'B')] = table_params(
-        WIDTH,
-        mLJ_pot_force_shifted,
-        r_min*sig_AB,
-        r_cutoff*sig_AB,
-        coeff=dict(epsilon=eps_AB, sigma=sig_AB, delta=delta)
-    )
-    table.r_cut[('A', 'B')] = r_cutoff*sig_AB
-    table.params[('B', 'B')] = table_params(
-        WIDTH,
-        mLJ_pot_force_shifted,
-        r_min*sig_BB,
-        r_cutoff*sig_BB,
-        coeff=dict(epsilon=eps_BB, sigma=sig_BB, delta=delta)
-    )
-    table.r_cut[('B', 'B')] = r_cutoff*sig_BB
-    return table
 
 # TODO need to update the table params to HOOMD v3
 def _Harmonic(nlist: hoomd.md.nlist.NList) -> hoomd.md.pair.Pair:

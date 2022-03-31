a6b36530ee0f5757facacc6f8da4232f2b478e26
Ian Graham <irgraham1@gmail.com>
2022-03-31 18:27:43 -0400

    removed explicit CC

--- build-base-hoomd.sh
+++ build-base-hoomd.sh
@@ -2,8 +2,6 @@
 
 rm -rf .build/hoomd
 
-export CC=/mnt/rrio1/home/opt/seas/pkg_sl79/gcc-7.3.0/gcc-7.3.0/bin/gcc
-
 cmake -B .build/hoomd -S hoomd-blue -DENABLE_GPU=$ENABLE_GPU -DBUILD_HPMC=OFF -DBUILD_METAL=OFF -DSINGLE_PRECISION=OFF -DENABLE_TBB=OFF -DCMAKE_CUDA_ARCHITECTURES=$CUDAARCHS -DCMAKE_BUILD_TYPE=Release
 
 cmake --build .build/hoomd -j$BUILD_THREADS

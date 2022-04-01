22246af2cf5598f2a7e33fdf6c478554290de7a6
Ian Graham <irgraham1@gmail.com>
2022-03-31 21:18:19 -0400

    start working on new workflows using signac and hydra

--- workflows/generate_random_states.py
+++ workflows/generate_random_states.py
@@ -2,3 +2,11 @@ import flow
 import hoomd
 import numpy as np
 import signac
+
+class LogTrigger(hoomd.trigger.Trigger):
+
+    def __init__(self):
+        hoomd.trigger.Trigger.__init__(self)
+
+    def compute(self, timestep):
+        return (timestep**(1 / 2)).is_integer()
